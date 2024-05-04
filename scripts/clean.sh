7221b5574288a17ec7e2925d12e9e273d7854e1e
muhme <github@heikol.de>
2024-05-04 18:39:33 +0200

    make clean.sh working for GNU xargs
    in setting -r to no run docker rm if input is empty as FreeBSD does

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -7,4 +7,4 @@
 # clean.sh - delete all quote_joomla_* docker containers
 
 echo '*** Removing all docker containers quote_joomla_*'
-docker ps -a --format '{{.Names}}' | grep '^quote_joomla_' | xargs docker rm -f
+docker ps -a --format '{{.Names}}' | grep '^quote_joomla_' | xargs -r docker rm -f
