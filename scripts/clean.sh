5eb15a0cbff495f6e382813b6bf77b5ecacf9c90
muhme <github@heikol.de>
2024-06-19 17:19:41 +0200

    Also delete the Docker network with clean

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -4,7 +4,17 @@
 # This software is licensed under the MIT License.
 # For the full license text, see the LICENSE file in the project root or visit https://opensource.org/licenses/MIT
 #
-# clean.sh - delete all quote_joomla_* docker containers
+# clean.sh - delete all quote_joomla_* docker containers and quote_joomla_default network
+#
+# or use simpe 'docker compose down'
 
-echo '*** Removing all docker containers quote_joomla_*'
-docker ps -a --format '{{.Names}}' | grep '^quote_joomla_' | xargs -r docker rm -f
+PREFIX="quote_joomla_"
+NETWORK_NAME="${PREFIX}default"
+
+echo "*** Remove following Docker containers"
+docker ps -a --format '{{.Names}}' | grep "^${PREFIX}" | xargs -r docker rm -f
+
+echo "*** Remove following Docker network"
+if docker network inspect "$NETWORK_NAME" >/dev/null 2>&1; then
+  docker network rm "$NETWORK_NAME"
+fi
