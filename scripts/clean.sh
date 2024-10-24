43578d61d503e56671fd9ad9a5721cbc3667aded
muhme <github@heikol.de>
2024-10-24 18:02:41 +0200

    Use docker compose down

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 #
-# Copyright (c) 2023 Heiko Lübbe
+# Copyright (c) 2023 - 2024 Heiko Lübbe
 # This software is licensed under the MIT License.
 # For the full license text, see the LICENSE file in the project root or visit https://opensource.org/licenses/MIT
 #
 # clean.sh - delete all quote_joomla_* docker containers and quote_joomla_default network
-#
-# or use simpe 'docker compose down'
 
 PREFIX="quote_joomla_"
 NETWORK_NAME="${PREFIX}default"
 
-echo "*** Remove following Docker containers"
-docker ps -a --format '{{.Names}}' | grep "^${PREFIX}" | xargs -r docker rm -f
+echo "*** Stopping and removing Docker containers, associated Docker networks and volumes"
+docker compose down -v
 
-echo "*** Remove following Docker network"
+remaining=$(docker ps -a --format '{{.Names}}' | grep "^${PREFIX}")
+if [ -n "${remaining}" ]; then
+  echo "*** Remove still existing Docker containers"
+  docker rm -f ${remaining}
+fi
+
 if docker network inspect "$NETWORK_NAME" >/dev/null 2>&1; then
+  echo "*** Remove still existing Docker network"
   docker network rm "$NETWORK_NAME"
 fi
