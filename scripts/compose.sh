44843491eeb294546cf21d8655e52fc2a50bcb51
muhme <github@heikol.de>
2024-10-25 05:35:03 +0200

    Using scripts/clean.sh

--- scripts/compose.sh
+++ scripts/compose.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #
-# Copyright (c) 2023 Heiko Lübbe
+# Copyright (c) 2023 - 2024 Heiko Lübbe
 # This software is licensed under the MIT License.
 # For the full license text, see the LICENSE file in the project root or visit https://opensource.org/licenses/MIT
 #
 # compose.sh - delete the six docker containers and build them new
 
-echo '*** Removing all docker containers quote_joomla_*'
-docker ps -a --format '{{.Names}}' | grep '^quote_joomla_' | xargs docker rm -f
+scripts/clean.sh
 
 if [ $# -eq 1 ] && [ "$1" = "build" ] ; then
   echo '*** Docker compose build --no-cache'
